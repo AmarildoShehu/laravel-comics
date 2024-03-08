a9954cf38e7f93d804d8380239478a707c7f2b1a
Amarildo <you@example.com>
2024-03-08 15:43:59 +0100

    added sass bootstrap and vite

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ env('APP_NAME'), 'DC Comics'}} | Home</title>
+
+    @vite('resources/js/app.js')
 </head>
 <body>
 <header>
